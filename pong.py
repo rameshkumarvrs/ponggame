3425ef3dcbac8362aed857ab0319b4ddaef86c7c
ramesh-exalogic <ramesh.kumar@exalogic.co>
2024-12-10 12:55:26 +0530

    added the side bar

--- pong.py
+++ pong.py
@@ -8,5 +8,14 @@ win.setup(800,600)
 win.bgcolor("blue")
 win.title("pong game")
 
+#left paddle
+left_padel = turtle.Turtle()
+left_padel.speed(0)
+left_padel.shape("square")
+left_padel.color("white")
+left_padel.shapesize(stretch_wid=5, stretch_len=1)
+left_padel.penup()
+left_padel.goto(-380,0)
+
 while True:
     win.update()
